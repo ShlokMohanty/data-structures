95b3b5ab1c661dab074c400ddfa4241a7e1ed7e8
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-08-22 23:29:18 +0530

    solved searching in rotated sorted array

---
+++ leetcode/Search_Rotated_sorted_array.java
@@ -0,0 +1,50 @@
+class Solution {
+   public int search(int[] nums, int target) {
+        int minimum = searchMinimum(nums);
+        if(nums[minimum] <= target && target <= nums[nums.length-1]){
+             return binarySearch(nums,minimum,nums.length-1,target);
+        }
+        else{
+            return binarySearch(nums,0,minimum,target);
+        }
+    }
+    
+    //binary search to find target in left to right boundary
+    public int binarySearch(int[] nums,int start,int end,int target){
+        int lo = start;
+        int high = end;
+        // System.out.println(left+" "+right);
+        while(lo <= high){
+            int mid = lo + (high - lo)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
+        return -1;
+    }
+    public int searchMinimum(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(mid > 0 && nums[mid-1] > nums[mid]){
+                return mid;
+            }
+            else if(nums[start] <= nums[mid] && nums[mid] > nums[end]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        
+        return start;
+    }
+    
+}
